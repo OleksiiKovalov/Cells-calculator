c499c6f0e360753f042dfee76397a0475cc67cba
Wertcars <pswertcar@gmail.com>
2026-05-28 15:20:39 +0300

    add test_local to run_all_tests.bat

--- scripts/run_all_tests.bat
+++ scripts/run_all_tests.bat
@@ -6,18 +6,56 @@ if "%conda_root%"=="" goto no_conda_found
 echo.
 echo Using conda environment: %condaname%
 
-echo Running tests in the repository...
+set FAILURE=0
+
+echo Running unit tests...
 call conda run -n "%condaname%" python -m pytest -v ../tests
-if errorlevel 1 (
+set EXITCODE=%ERRORLEVEL%
+if %EXITCODE% NEQ 0 (
     echo.
-    echo One or more tests failed.
-    goto exit_with_error
+    echo Unit tests FAILED.
+    set FAILURE=1
+) else (
+    echo.
+    echo Unit tests passed.
+)
+
+    echo.
+    echo Running smoke tests...
+    call conda run -n "%condaname%" python -m pytest -v ../tests_local/test_smoke.py
+    set EXITCODE=%ERRORLEVEL%
+    if %EXITCODE% NEQ 0 (
+        echo.
+        echo Smoke tests FAILED.
+        set FAILURE=1
+    ) else (
+        echo.
+        echo Smoke tests passed.
+    )
+
+echo.
+echo Running golden regression tests...
+call conda run -n "%condaname%" python -m pytest -v ../tests_local/test_image_golden_regressions.py
+set EXITCODE=%ERRORLEVEL%
+if %EXITCODE% NEQ 0 (
+    echo.
+    echo Golden regression tests FAILED.
+    set FAILURE=1
+) else (
+    echo.
+    echo Golden regression tests passed.
 )
 
 echo.
-echo All available tests completed successfully.
-pause
-goto exit
+if "%FAILURE%"=="0" (
+    echo All available tests completed successfully.
+    pause
+    goto exit
+) else (
+    echo One or more test suites failed.
+    pause
+    goto exit_with_error
+)
 
 :no_conda_found
 echo NO ANACONDA/MINICONDA FOUND!
@@ -36,5 +74,3 @@ echo.
 echo Press any key to close.
 pause
 exit /b 0
-
-
