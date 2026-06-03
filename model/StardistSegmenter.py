ebda6ccbcf372200c9abdea4facdbeab51aa86db
John Kharkivskyi <gkharkovskiy@gmail.com>
2026-06-03 21:09:24 +0300

    Fix Stardist test fail

--- model/StardistSegmenter.py
+++ model/StardistSegmenter.py
@@ -127,8 +127,22 @@ class StardistSegmenter(BaseModel):
        
         self.original_image = safegray2rgb(image)
         try:
+            import stardist.models.model2d
+            import numpy as np
+            original_nms = stardist.models.model2d.non_maximum_suppression_sparse
+            def safe_nms_sparse(dist, prob, points, *args, **kwargs):
+                # Clip distances to prevent C++ NMS crash on extreme values (e.g. >1e22)
+                dist = np.clip(dist, a_min=None, a_max=10000.0)
+                return original_nms(dist, prob, points, *args, **kwargs)
+            
+            stardist.models.model2d.non_maximum_suppression_sparse = safe_nms_sparse
+            
             labels, details = None, None
-            labels, details = self.model.predict_instances(img_inference)
+            try:
+                labels, details = self.model.predict_instances(img_inference)
+            finally:
+                stardist.models.model2d.non_maximum_suppression_sparse = original_nms
+
             self.detections = self.stardist_results_to_pandas(
                 labels,
                 scores=details["prob"],
